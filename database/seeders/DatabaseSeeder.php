d639c3daba492138c41c31c6c8b8f581bb3d97b5
Job <jobmoreno98@gmail.com>
2024-09-25 20:27:20 -0600

    correccion migraciones 3.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
 
         DB::table('model_has_roles')->insert([
             'role_id' => 1,
-            'model_type ' => 'App\Models\User',
+            'model_type' => 'App\Models\User',
             'model_id' => 1
         ]);
     }
